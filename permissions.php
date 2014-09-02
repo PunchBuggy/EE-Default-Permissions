e3a73541348b6c347578d55b4c6ac180f31bb308
Nicholas Powell <nicholas@punchbuggy.com.au>
2014-09-03 09:48:40 +1000

    Adds cp name detection and styles

--- permissions.php
+++ permissions.php
@@ -1,34 +1,54 @@
 <html>
 	<head>
 		<title>PB Expressionengine Permissions Script</title>
+		<style type="text/css">
+			table td {padding-right:12px;}
+			table tbody td:first-child {font-weight:bold;}
+		</style>
 	</head>
 	<body>
-		<?php
-			function setPermissions($file, $mode) {
-				$success = chmod($file, $mode);
-				echo "<p style='font-weight:bold;color:" . ($success? '#3c763d' : '#a94442') . ";'>" . $file . ': ';
-				echo ($success? 'Success' : 'Failed') . '</p>';
-			}
+		<table>
+			<thead>
+				<tr>
+					<td>Status</td>
+					<td>File</td>
+					<td>Permission</td>
+				</tr>
+			</thead>
+			<tbody>
+				<?php			
+					function setPermissions($file, $mode) {
+						$success = chmod($file, $mode);
+						echo "<tr>";
+						echo "<td style='color:" . ($success? '#3c763d' : '#a94442') . ";'>" . ($success? 'Success' : 'Failed') . "</td><td>" . $file . "</td><td>" . $mode . "</td>";
+						echo "</tr>";
+					}
+					
+					if (file_exists("control-panel/")) {
+						$prefix = "control-panel";
+					} elseif (file_exists("system/")) {
+						$prefix = "system";
+					}
 
-			$prefix = "system";
-
-			if (file_exists($prefix . '/')) {
-				echo "Found " . $prefix;
-				// 666
-				setPermissions($prefix . "/expressionengine/config/config.php", 0666);
-				setPermissions($prefix . "/expressionengine/config/database.php", 0666);
-				// 777
-				setPermissions($prefix . "/expressionengine/cache/", 0777);
-				setPermissions("images/avatars/uploads/", 0777);
-				setPermissions("images/captchas/", 0777);
-				setPermissions("images/member_photos/", 0777);
-				setPermissions("images/pm_attachments/", 0777);
-				setPermissions("images/signature_attachments/", 0777);
-				setPermissions("images/uploads/", 0777);
-			} else {
-				echo "Configuration files not found";
-			}
-		?>
+					if (file_exists($prefix . '/')) {
+						echo "Found " . $prefix;
+						// 666
+						setPermissions($prefix . "/expressionengine/config/config.php", 666);
+						setPermissions($prefix . "/expressionengine/config/database.php", 666);
+						// 777
+						setPermissions($prefix . "/expressionengine/cache/", 777);
+						setPermissions("images/avatars/uploads/", 777);
+						setPermissions("images/captchas/", 777);
+						setPermissions("images/member_photos/", 777);
+						setPermissions("images/pm_attachments/", 777);
+						setPermissions("images/signature_attachments/", 777);
+						setPermissions("images/uploads/", 777);
+					} else {
+						echo "Control panel folder not found";
+					}
+				?>
+			</tbody>
+		</table>
 	</body>
 </html>
 
